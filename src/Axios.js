b5c45026460a48785e9caa0ee000aefbf9f0321e
Hilman <hilman@loyalto.id>
2024-07-02 18:16:53 +0700

    handle logout

--- src/Axios.js
+++ src/Axios.js
@@ -8,18 +8,28 @@ const api = axios.create({
 });
 
 export const login = async (merchantID, token) => {
-    try {
-      const response = await api.post('/pgqr/loginQr', { merchantID, token});
-      // Simpan data ke localStorage
-      localStorage.setItem('merchantID', response.data.data.merchantID);
-      localStorage.setItem('cashierIndex', response.data.data.cashierIndex);
-      localStorage.setItem('cashierToken', response.data.data.cashierToken);
-      localStorage.setItem('apiKey', response.data.data.apiKey);
-      return response.data;
-    } catch (error) {
-      console.error('Error during login:', error);
-      throw error;
-    }
-  };
+  try {
+    const response = await api.post('/pgqr/loginQr', { merchantID, token });
+    // Simpan data ke localStorage
+    localStorage.setItem('merchantID', response.data.data.merchantID);
+    localStorage.setItem('cashierIndex', response.data.data.cashierIndex);
+    localStorage.setItem('cashierToken', response.data.data.cashierToken);
+    localStorage.setItem('apiKey', response.data.data.apiKey);
+    return response.data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
+
+export const logout = async (merchantID, index, token) => {
+  try {
+    const response = await api.post('/pgqr/loginQr/logout', { merchantID, index, token });
+    return response.data;
+  } catch (error) {
+    console.error('Error during login:', error);
+    throw error;
+  }
+};
 
 export default api;
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,33 @@
 // Home.js
+import { logout } from './Axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css'; // File CSS untuk gaya tambahan
 
 const Home = () => {
   const navigate = useNavigate();
+  const [merchantID, setMerchantID] = useState('');
+  const [cashierIndex, setCashierIndex] = useState('');
+  const [cashierToken, setCashierToken] = useState('');
 
   useEffect(() => {
-    const merchantID = localStorage.getItem('merchantID');
-    if (!merchantID) {
+    const storedMerchantID = localStorage.getItem('merchantID');
+    const storedCashierIndex = localStorage.getItem('cashierIndex');
+    const storedCashierToken = localStorage.getItem('cashierToken');
+
+    if (!storedMerchantID) {
       navigate('/login');
       return;
     }
-  });
 
-  const handleLogout = () => {
+    setMerchantID(storedMerchantID);
+    setCashierIndex(storedCashierIndex);
+    setCashierToken(storedCashierToken);
+  }, [navigate]);
+
+  const handleLogout = async () => {
+    const response = await logout(merchantID, cashierIndex, cashierToken);
+    console.log('Login successful:', response);
     localStorage.clear();
     navigate('/login');
   };
@@ -22,11 +35,14 @@ const Home = () => {
   return (
     <div className="home-container">
       <h2>Welcome to Home Page</h2>
+
       <button className="logout-button" onClick={handleLogout}>
         Logout
       </button>
-      <p>Merchant ID: {localStorage.getItem('merchantID')}</p>
-      <p>Cashier Index: {localStorage.getItem('cashierIndex')}</p>
+
+      <p>Merchant ID: {merchantID}</p>
+      <p>Cashier Index: {cashierIndex}</p>
+      <p>Cashier Token: {cashierToken}</p>
     </div>
   );
 };
